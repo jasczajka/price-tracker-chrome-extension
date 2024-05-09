526efba2ec5b2ad188aa67d268467a0fc02bc993
jasczajka <czajka.jan12@gmail.com>
2024-05-09 18:36:16 +0200

    two selectors at once functionality

--- content.js
+++ content.js
@@ -1,26 +1,55 @@
-let isCaptureSelectorEnabled = false;
-
-
+let isRegularCaptureSelectorEnabled = false;
+let isDiscountedCaptureSelectorEnabled = false;
+let isRegularSelectorChosen = false;
+let isDiscountedSelectorChosen = false;
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){
         console.log(request.greeting)
-        if (request.greeting === "enable_selector"){
-            console.log("selector enabled")
+        if (request.greeting === "enable_selector_regular"){
+          //check if the other is not enabled or if this one is not chosen
+          if(!isDiscountedCaptureSelectorEnabled && !isRegularSelectorChosen){
+              console.log("regular selector enabled")
+              document.addEventListener('click',selectionClickListener)
+              document.addEventListener('mouseover', addHoverEffect)
+              document.addEventListener('mouseout', removeHoverEffect)
+              isRegularCaptureSelectorEnabled = true;
+          }
+        }
+        if (request.greeting === "enable_selector_discounted"){
+          //check if the other is not enabled or if this one is not chosen
+          if(!isRegularCaptureSelectorEnabled && !isDiscountedSelectorChosen){
+            console.log("discounted selector enabled")
             document.addEventListener('click',selectionClickListener)
             document.addEventListener('mouseover', addHoverEffect)
             document.addEventListener('mouseout', removeHoverEffect)
-            
-            isCaptureSelectorEnabled = true;
-        }
+            isDiscountedCaptureSelectorEnabled = true;
+          }
+      }
     }
 )
 function selectionClickListener(e) {
-  if (isCaptureSelectorEnabled){
+  if (isDiscountedCaptureSelectorEnabled || isRegularCaptureSelectorEnabled){
     const selector = getCSSSelector(e.target)
-    document.querySelector(selector).style.border = '2px solid green'
-    chrome.runtime.sendMessage({greeting:'new_selector',selector:selector})
+    
+
+    const greeting = isDiscountedCaptureSelectorEnabled? 'new_selector_regular':'new_selector_discounted'
+    chrome.runtime.sendMessage({greeting:greeting,selector:selector})
+    
+    if(isRegularCaptureSelectorEnabled){
+      console.log("regular price chosen")
+      e.target.classList.remove('hovered')
+      e.target.classList.add('regular_price')
+      isRegularSelectorChosen = true
+    }
+    if(isDiscountedCaptureSelectorEnabled) {
+      console.log("discounted price chosen")
+      e.target.classList.remove('hovered')
+      e.target.classList.add('discounted_price')
+      isDiscountedSelectorChosen = true
+    }
+    isRegularCaptureSelectorEnabled = false
+    isDiscountedCaptureSelectorEnabled = false
     console.log("selector disabled")
-    isCaptureSelectorEnabled = false
     document.removeEventListener('mouseover', addHoverEffect)
     document.removeEventListener('mouseout', removeHoverEffect)
     document.removeEventListener('click',selectionClickListener)
@@ -39,13 +68,15 @@ function removeHoverEffect (event ){
 
 
 function getCSSSelector(el){
-    let selector = el.tagName.toLowerCase();
-    const attrs = el.attributes
-    for (var i = 0; i < attrs.length; i++) {
-        let attr = attrs.item(i)
-        if (attr.name === 'id') selector += `#${attr.value}`;
-        if (attr.name === 'class') selector += attr.value.split(' ').map((c) => `.${c}`).join('');
-        if (attr.name === 'name') selector += `[${attr.name}=${attr.value}]`;
-    }
-    return selector
+  let selector = el.tagName.toLowerCase();
+  const attrs = el.attributes
+  for (var i = 0; i < attrs.length; i++) {
+      let attr = attrs.item(i)
+      if (attr.name === 'id') selector += `#${attr.value}`;
+      if (attr.name === 'class') selector += attr.value.split(' ')
+      .filter(c => c !== 'regular_price' && c !== 'discounted_price')
+      .map((c) => `.${c}`).join('');
+      if (attr.name === 'name') selector += `[${attr.name}=${attr.value}]`;
+  }
+  return selector
 }
--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', async function() {
-  // Add event listener for click on the fetch button
   document.getElementById('fetchButton').addEventListener('click', function() {
     fetchData();
   });
@@ -8,17 +7,39 @@ document.addEventListener('DOMContentLoaded', async function() {
 
 
 document.addEventListener('DOMContentLoaded', ()=> {
-  document.getElementById('getSelectorButton').addEventListener('click',  () => {
+  document.getElementById('indicateRegularPriceButton').addEventListener('click',  () => {
     chrome.tabs.query({active: true, lastFocusedWindow: true,},  (tabs) => {
       const tabId = tabs[0].id
       console.log(tabId)
-      chrome.tabs.sendMessage(tabId,{greeting:"enable_selector"})
+      chrome.tabs.sendMessage(tabId,{greeting:"enable_selector_regular"})
+    })
+  });
+});
+document.addEventListener('DOMContentLoaded', ()=> {
+  document.getElementById('indicateDiscountedPriceButton').addEventListener('click',  () => {
+    chrome.tabs.query({active: true, lastFocusedWindow: true,},  (tabs) => {
+      const tabId = tabs[0].id
+      console.log(tabId)
+      chrome.tabs.sendMessage(tabId,{greeting:"enable_selector_discounted"})
     })
   });
 });
 
 
-
 async function fetchData(){
   await chrome.runtime.sendMessage({greeting:'fetch_prices'})
+}
+
+function getCSSSelector(el){
+  let selector = el.tagName.toLowerCase();
+  const attrs = el.attributes
+  for (var i = 0; i < attrs.length; i++) {
+      let attr = attrs.item(i)
+      if (attr.name === 'id') selector += `#${attr.value}`;
+      if (attr.name === 'class') selector += attr.value.split(' ')
+      .filter(c => c !== 'regular_price' && c !== 'discounted_price')
+      .map((c) => `.${c}`).join('');
+      if (attr.name === 'name') selector += `[${attr.name}=${attr.value}]`;
+  }
+  return selector
 }
